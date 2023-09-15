3e0ff5b66ada3534b32faec6e61eea9994d0e6c3
Oleksii Tkachenko <zlukapraporr@gmail.com>
2023-09-16 02:45:20 +0300

    ADD class

---
+++ Java_EmployeePract/src/main/java/sumdu/edu/ua/java_employeepract/IncorrectSalaryException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sumdu.edu.ua.java_employeepract;
+
+/**
+ *
+ * @author Oleksii
+ */
+
+public class IncorrectSalaryException extends Exception {
+    public IncorrectSalaryException(String message) {
+        super(message);
+    }
+}
