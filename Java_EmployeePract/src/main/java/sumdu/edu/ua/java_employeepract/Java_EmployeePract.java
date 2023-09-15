ec8a090f59223ea84f9f9feb0b2d6da0eab84259
Oleksii Tkachenko <zlukapraporr@gmail.com>
2023-09-16 01:27:37 +0300

    init start ok

---
+++ Java_EmployeePract/src/main/java/sumdu/edu/ua/java_employeepract/Java_EmployeePract.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sumdu.edu.ua.java_employeepract;
+
+/**
+ *
+ * @author Oleksii
+ */
+public class Java_EmployeePract {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
