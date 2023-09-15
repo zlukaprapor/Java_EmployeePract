9d9757847f4dd221cab78fb887f74b8e55c78ec5
Oleksii Tkachenko <zlukapraporr@gmail.com>
2023-09-16 02:45:10 +0300

    ADD class

---
+++ Java_EmployeePract/src/main/java/sumdu/edu/ua/java_employeepract/FieldLengthLimitException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sumdu.edu.ua.java_employeepract;
+
+/**
+ *
+ * @author Oleksii
+ */
+public class FieldLengthLimitException extends Exception {
+    public FieldLengthLimitException(String message) {
+        super(message);
+    }
+}
