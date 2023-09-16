3b39716805373606295cf50a7c481a5a689df6f5
Oleksii Tkachenko <zlukapraporr@gmail.com>
2023-09-17 02:32:13 +0300

    Finish 2

--- Java_EmployeePract/src/main/java/sumdu/edu/ua/java_employeepract/Java_EmployeePract.java
+++ Java_EmployeePract/src/main/java/sumdu/edu/ua/java_employeepract/Java_EmployeePract.java
@@ -3,8 +3,6 @@
  */
 package sumdu.edu.ua.java_employeepract;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -14,7 +12,8 @@ public class Java_EmployeePract {
 
     public static void main(String[] args) {
         try {
-            Employee emp1 = new Employee("Taras", "Shevchenko", 1345);
+            Employee emp1;
+            emp1 = new Employee("John", "Doe", 5000);
             emp1.PrintEmployeeId();
             System.out.println("Name - " + emp1.getName());
             System.out.println("Surname - " + emp1.getSurname());
@@ -26,58 +25,7 @@ public class Java_EmployeePract {
             System.out.println("Incorrect Salary Exception:  " + ex.getMessage());
         }
 
-        try {
-            Employee emp2 = new Employee("Tarassssssssssssssssssssssssssssssss", "Shevchenko", 1345);
-            emp2.PrintEmployeeId();
-            System.out.println("Name - " + emp2.getName());
-            System.out.println("Surname - " + emp2.getSurname());
-            System.out.println("Salary - " + emp2.getSalary());
-
-        } catch (FieldLengthLimitException ex) {
-            System.out.println("Field Length Limit Exception: " + ex.getMessage());
-        } catch (IncorrectSalaryException ex) {
-            System.out.println("Incorrect Salary Exception:  " + ex.getMessage());
-        }
-
-        try {
-            Employee emp3 = new Employee("Taras", "Shevchenko0000000000000000000000000000000", 1345);
-            emp3.PrintEmployeeId();
-            System.out.println("Name - " + emp3.getName());
-            System.out.println("Surname - " + emp3.getSurname());
-            System.out.println("Salary - " + emp3.getSalary());
-
-        } catch (FieldLengthLimitException ex) {
-
-            System.out.println("Field Length Limit Exception: " + ex.getMessage());
-        } catch (IncorrectSalaryException ex) {
-            System.out.println("Incorrect Salary Exception:  " + ex.getMessage());
-        }
-
-        try {
-            Employee emp4 = new Employee("Taras", "Shevchenko", -1345);
-            emp4.PrintEmployeeId();
-            System.out.println("Name - " + emp4.getName());
-            System.out.println("Surname - " + emp4.getSurname());
-            System.out.println("Salary - " + emp4.getSalary());
-
-        } catch (FieldLengthLimitException ex) {
-            System.out.println("Field Length Limit Exception: " + ex.getMessage());
-        } catch (IncorrectSalaryException ex) {
-            System.out.println("Incorrect Salary Exception:  " + ex.getMessage());
-        }
-
-        try {
-            Employee emp5 = new Employee("Taras", "Shevchenko", 1345);
-            emp5.PrintEmployeeId();
-            System.out.println("Name - " + emp5.getName());
-            System.out.println("Surname - " + emp5.getSurname());
-            System.out.println("Salary - " + emp5.getSalary());
-
-        } catch (FieldLengthLimitException ex) {
-            System.out.println("Field Length Limit Exception: " + ex.getMessage());
-        } catch (IncorrectSalaryException ex) {
-            System.out.println("Incorrect Salary Exception:  " + ex.getMessage());
-        }
+       
 
     }
 }
